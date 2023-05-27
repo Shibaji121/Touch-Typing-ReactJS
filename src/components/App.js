caaa995e9bfca9de8c75274f3219240354de6084
shiba_1234 <shibajisahu121@gmail.com>
2023-05-27 18:49:40 +0530

    intial commit

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import Touchtyping from "./touchtyping";
+
+function App() {
+  const [expectedVal, setExpectedVal] = useState("");
+
+  useEffect(() => {
+    const letters = "asdfjkl";
+    const bigrams = [];
+    for (let i = 0; i < 6; i++) {
+      const randomIndex = Math.floor(Math.random() * (letters.length - 1));
+      const firstLetter = letters[randomIndex];
+      const secondLetter = letters[randomIndex + 1];
+      const bigram = `${firstLetter}${secondLetter}`;
+      bigrams.push(bigram);
+    }
+    const randomSent = bigrams.join(" ");
+    console.log(randomSent);
+    setExpectedVal(randomSent);
+  }, []);
+
+  return (
+    <div className="App">
+      <Touchtyping expectedVal={expectedVal} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/touchtyping.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+
+export default function Touchtyping(props) {
+  const [timer, setTimer] = useState(10);
+  const [keysPressed, setKeysPressed] = useState(0);
+  const [isTimer, setIsTimer] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [nextExpKey, setnextExpKey] = useState(0);
+
+  useEffect(() => {
+    let interval;
+    if (isTimer) {
+      interval = setInterval(() => {
+        setTimer((prevTime) => prevTime - 1);
+      }, 1000);
+    }
+    if (timer === 0) {
+      setIsTimer(false);
+    }
+    return () => clearInterval(interval);
+  }, [isTimer, timer]);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    setKeysPressed((prevCount) => prevCount + 1);
+    if (e.target.value.length < inputValue.length) {
+      console.log("back space");
+    }
+    if (inputValue === props.expectedVal) {
+      alert("Completed");
+    }
+    if (e.target.value.slice(-1) !== props.expectedVal.charAt(nextExpKey)) {
+      console.log("You entered wrong letter");
+      setnextExpKey((prev) => prev - 1);
+    } else {
+      setnextExpKey((prev) => prev + 1);
+    }
+    setIsTimer(true);
+  };
+
+  return (
+    <div className="typing-app">
+      <div className="timer">
+        Timer:{" "}
+        {Math.floor(timer / 60)
+          .toString()
+          .padStart(2, "0")}
+        :{(timer % 60).toString().padStart(2, "0")}
+      </div>
+      <div className="measurements">
+        <div className="wpm measure-box">
+          <h2>WPM: 40</h2>
+        </div>
+        <div className="accuracy measure-box">
+          <h2>Accuracy: 100%</h2>
+        </div>
+        <div className="key-pressed measure-box">
+          <h2>Key Pressed: {keysPressed}</h2>
+        </div>
+      </div>
+      <div className="expected-phrase">{props.expectedVal}</div>
+      <input
+        value={inputValue}
+        className="typing-input"
+        placeholder="Enter as per the above shown sentence"
+        onChange={handleInputChange}
+        autoFocus
+      />
+    </div>
+  );
+}
